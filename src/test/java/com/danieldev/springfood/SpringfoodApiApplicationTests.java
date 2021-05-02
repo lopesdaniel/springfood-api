61d4f0a2a637ffc2c390d90ece3108ad25f0155c
Daniel Lopes <viana.daniel02@gmail.com>
2021-05-02 00:10:52 -0300

    Commit inicial

---
+++ src/test/java/com/danieldev/springfood/SpringfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieldev.springfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfoodApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
